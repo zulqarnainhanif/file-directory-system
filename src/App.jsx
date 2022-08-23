c73f7cafee0c773a85419ed0e4ead9ad89c269db
Zulqarnain Hanif <30355034+zulqarnainhanif@users.noreply.github.com>
2022-08-23 19:25:12 +0500

    remove MUI imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import "./App.css";
 import React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import { Container, List } from "@mui/material";
-import Directory from "./components/Directory";
+
 import { SnackbarProvider } from "notistack";
+
+import "./App.css";
+import Directory from "./components/Directory";
 
 function App() {
   return (
     <SnackbarProvider maxSnack={3}>
-      <CssBaseline />
       <nav className="App-navbar">File Directory System</nav>
-      <Container className="App-container">
-        <List component="div" disablePadding>
-          <Directory name="root" path="root" />
-        </List>
-      </Container>
+      <div className="App-container">
+        <Directory name="root" path="root" />
+      </div>
     </SnackbarProvider>
   );
 }
