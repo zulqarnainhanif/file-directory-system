df2cfaf8eb01875c5e8836e516a013bde8997f43
Zulqarnain Hanif <zulqarnain.haneef@gmail.com>
2022-08-25 14:28:33 +0500

    add search feature in directory

--- src/components/Directory/index.jsx
+++ src/components/Directory/index.jsx
@@ -11,23 +11,41 @@ import { ReactComponent as ExpandLess } from "../../assets/icons/ExpandLess.svg"
 const Directory = (props) => {
   const { name, path } = props;
   const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState("");
   const [details, setDetails] = useState(null);
 
   const handleClick = () => {
-    getFileDirectory(path)
-      .then((res) => {
-        setDetails(res);
-        setOpen(!open);
-      })
-      .catch((error) => {
-        props.enqueueSnackbar(
-          error?.message || "Failed to fetch Directory details!",
-          { variant: "error" }
-        );
-      });
+    if (!open && !details) {
+      getFileDirectory(path)
+        .then((res) => {
+          setDetails(res);
+          setOpen(!open);
+        })
+        .catch((error) => {
+          props.enqueueSnackbar(
+            error?.message || "Failed to fetch Directory details!",
+            { variant: "error" }
+          );
+        });
+    } else {
+      setOpen(!open);
+    }
+  };
+
+  const handleSearchTextChange = (e) => {
+    e.stopPropagation();
+    setSearch(e.target.value);
   };
 
   const haveDetails = details?.entries?.length > 0;
+
+  const filteredEnteries =
+    search !== ""
+      ? haveDetails &&
+        details?.entries.filter((dir) =>
+          dir.name.includes(search.trim().toLowerCase())
+        )
+      : details?.entries;
 
   return (
     <Fragment>
@@ -35,27 +53,47 @@ const Directory = (props) => {
         {open && <ExpandLess />}
         {!open && <ExpandMore />}
         <div>{name}</div>
+        {open && haveDetails && (
+          <div
+            className="Directory-searchbar"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <input
+              value={search}
+              placeholder="Enter search text here..."
+              onChange={handleSearchTextChange}
+            />
+          </div>
+        )}
       </div>
       {haveDetails && open && (
-        <div className="Directory-details">
-          {details?.entries?.map((dir, index) =>
-            dir.type === "directory" ? (
-              <Directory
-                key={index}
-                name={dir.name}
-                enqueueSnackbar={props.enqueueSnackbar}
-                path={path === "root" ? dir.name : path + "/" + dir.name}
-              />
-            ) : (
-              <File
-                key={index}
-                name={dir.name}
-                enqueueSnackbar={props.enqueueSnackbar}
-                path={path === "root" ? dir.name : path + "/" + dir.name}
-              />
-            )
+        <>
+          {filteredEnteries.length ? (
+            <div className="Directory-details">
+              {filteredEnteries.map((dir, index) =>
+                dir.type === "directory" ? (
+                  <Directory
+                    key={index}
+                    name={dir.name}
+                    enqueueSnackbar={props.enqueueSnackbar}
+                    path={path === "root" ? dir.name : path + "/" + dir.name}
+                  />
+                ) : (
+                  <File
+                    key={index}
+                    name={dir.name}
+                    enqueueSnackbar={props.enqueueSnackbar}
+                    path={path === "root" ? dir.name : path + "/" + dir.name}
+                  />
+                )
+              )}
+            </div>
+          ) : (
+            <div className="Directory-details">No record found</div>
           )}
-        </div>
+        </>
       )}
     </Fragment>
   );
